d64321459c2e57b79f6f756646caa97397e6a1be
Stefan Scheidt <stefan.scheidt@icloud.com>
2019-12-04 13:55:19 +0100

    Add day 3 task and solution of part 1.

---
+++ src/main/kotlin/adventofcode/day03/Day03.kt
@@ -0,0 +1,61 @@
+package adventofcode.day03
+
+import adventofcode.day03.Move.*
+import java.io.File
+import kotlin.math.abs
+
+
+data class Point(val x: Int, val y: Int) {
+
+    operator fun plus(other: Point) = copy(x = x + other.x, y = y + other.y)
+
+    fun dist(other: Point = Point(0, 0)): Int = abs(x - other.x) + abs(y - other.y)
+
+}
+
+typealias Vector = Point
+
+typealias Trace = List<Point>
+
+sealed class Move(val vector: Vector, val steps: Int) {
+
+    class Up(steps: Int) : Move(Vector(0, 1), steps)
+    class Down(steps: Int) : Move(Vector(0, -1), steps)
+    class Left(steps: Int) : Move(Vector(-1, 0), steps)
+    class Right(steps: Int) : Move(Vector(1, 0), steps)
+
+    fun generateTrace(start: Point): Trace =
+        generateSequence(start + vector) { pt -> pt + vector }.take(steps).toList()
+
+}
+
+fun String.toMove(): Move {
+    if (this.length < 2) throw IllegalArgumentException("invalid move $this")
+    val code = this[0]
+    val steps = this.substring(1).toInt()
+    return when (code) {
+        'U'  -> Up(steps)
+        'D'  -> Down(steps)
+        'L'  -> Left(steps)
+        'R'  -> Right(steps)
+        else -> throw IllegalArgumentException("invalid move code $code")
+    }
+}
+
+fun String.trace(): Trace =
+    split(",")
+        .map(String::toMove)
+        .fold(initialTrace()) { acc, move -> acc + move.generateTrace(acc.last()) }
+
+private fun initialTrace(): Trace =
+    listOf(Point(0, 0))
+
+fun distToNearestIntersect(tr1: Trace, tr2: Trace): Int? =
+    tr1.drop(1).intersect(tr2.drop(1)).map { it.dist() }.min()
+
+fun main() {
+    val lines = File("./input/day03.txt").readLines()
+    val tr1 = lines[0].trace()
+    val tr2 = lines[1].trace()
+    println(distToNearestIntersect(tr1, tr2))
+}
--- src/test/kotlin/adventofcode/day03/Day03Test.kt
+++ src/test/kotlin/adventofcode/day03/Day03Test.kt
@@ -1,3 +1,41 @@
 package adventofcode.day03
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.io.File
 
+
+class TraceTest {
+
+    @Test
+    fun `compute trace`() {
+        assertThat("U1".trace())
+            .containsExactly(Point(0, 0), Point(0, 1))
+        assertThat("U2,R2".trace())
+            .containsExactly(Point(0, 0), Point(0, 1), Point(0, 2), Point(1, 2), Point(2, 2))
+    }
+
+    @Test
+    fun `distance of nearest intersection`() {
+        val tr11 = "R8,U5,L5,D3".trace()
+        val tr12 = "U7,R6,D4,L4".trace()
+        assertThat(distToNearestIntersect(tr11, tr12)).isEqualTo(6)
+
+        val tr21 = "R75,D30,R83,U83,L12,D49,R71,U7,L72".trace()
+        val tr22 = "U62,R66,U55,R34,D71,R55,D58,R83".trace()
+        assertThat(distToNearestIntersect(tr21, tr22)).isEqualTo(159)
+
+        val tr31 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".trace()
+        val tr32 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".trace()
+        assertThat(distToNearestIntersect(tr31, tr32)).isEqualTo(135)
+    }
+
+    @Test
+    fun `solution of part 1`() {
+        val lines = File("./input/day03.txt").readLines()
+        val tr1 = lines[0].trace()
+        val tr2 = lines[1].trace()
+        assertThat(distToNearestIntersect(tr1, tr2)).isEqualTo(1674)
+    }
+
+}

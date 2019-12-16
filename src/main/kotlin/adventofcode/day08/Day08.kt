0f2c2b2d9900521335911e9ed7568a2d402c5431
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2019-12-16 21:55:12 +0100

    Day 8, part two.

--- src/main/kotlin/adventofcode/day08/Day08.kt
+++ src/main/kotlin/adventofcode/day08/Day08.kt
@@ -20,10 +20,25 @@ fun layerWithFewestDigit(layers: List<Layer>, digit: Int): Layer? =
         .minBy { it.value }
         ?.key
 
+fun render(image: Image, width: Int, height: Int): String =
+    image
+        .map { it.flatten() }
+        .fold(List(width * height) { 2 }) { acc, layer ->
+            acc.zip(layer).map { (a, l) -> if (a == 2) l else a }
+        }
+        .map { if (it == 0) ' ' else '*' }
+        .chunked(width)
+        .joinToString(separator = System.lineSeparator()) {
+            it.joinToString(separator = "")
+        }
+
+
 fun main() {
     val input = File("./input/day08.txt").readText()
     val image = input.toImage(25, 6)
-    val layer = layerWithFewestDigit(image, 0)!!
 
+    val layer = layerWithFewestDigit(image, 0)!!
     println("Part 1: ${digitCount(layer, 1) * digitCount(layer, 2)}")
+
+    println(render(image, 25, 6))
 }
--- src/test/kotlin/adventofcode/day08/Day08Test.kt
+++ src/test/kotlin/adventofcode/day08/Day08Test.kt
@@ -16,5 +16,21 @@ class Day08Test {
         assertThat(digitCount(layer, 1) * digitCount(layer, 2)).isEqualTo(1820)
     }
 
+    @Test
+    fun `part two`() {
+        val input = File("./input/day08.txt").readText()
+        val image = input.toImage(25, 6)
+        val zukcj = """
+            **** *  * *  *  **    ** 
+               * *  * * *  *  *    * 
+              *  *  * **   *       * 
+             *   *  * * *  *       * 
+            *    *  * * *  *  * *  * 
+            ****  **  *  *  **   **  
+        """.trimIndent()
+
+        assertThat(render(image, 25, 6)).isEqualTo(zukcj)
+    }
+
 }
 

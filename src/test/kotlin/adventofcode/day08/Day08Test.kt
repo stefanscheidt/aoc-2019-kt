6b6f6df13de346f056a6696cb8eedb09c2417d61
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2019-12-16 21:06:08 +0100

    Day 8, part one.

---
+++ src/test/kotlin/adventofcode/day08/Day08Test.kt
@@ -0,0 +1,20 @@
+package adventofcode.day08
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.io.File
+
+
+class Day08Test {
+
+    @Test
+    fun `part one`() {
+        val input = File("./input/day08.txt").readText()
+        val image = input.toImage(25, 6)
+        val layer = layerWithFewestDigit(image, 0)!!
+
+        assertThat(digitCount(layer, 1) * digitCount(layer, 2)).isEqualTo(1820)
+    }
+
+}
+

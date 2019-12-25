1e47984ff38ef14a44d84ee50d792648f27186a9
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2019-12-25 23:11:05 +0100

    Day 11, part two.

--- src/main/kotlin/adventofcode/day11/Day11.kt
+++ src/main/kotlin/adventofcode/day11/Day11.kt
@@ -13,6 +13,7 @@ import java.util.concurrent.TimeUnit
 typealias Color = Long
 
 const val BLACK = 0L
+const val WHITE = 1L
 
 enum class Direction(val vector: Vector) {
     NORTH(vect(0, -1)),
@@ -40,7 +41,7 @@ enum class Direction(val vector: Vector) {
 
 typealias Painting = Map<Coordinate, Color>
 
-class Robot(private val brain: QueueComputer) {
+class Robot(private val brain: QueueComputer, private val initialColor: Color) {
 
     private val paintedFields = mutableMapOf<Coordinate, Color>()
     val painting: Painting
@@ -52,6 +53,7 @@ class Robot(private val brain: QueueComputer) {
         get() = paintedFields[currentPosition] ?: BLACK
 
     fun start() {
+        paintedFields[currentPosition] = initialColor
         brain.putInput(currentColor)
         brain.runAsync()
         log(this, "poll next output")
@@ -74,11 +76,29 @@ class Robot(private val brain: QueueComputer) {
 
 }
 
+fun renderPainting(painting: Painting): String {
+    val minX = painting.keys.map { it.x }.min() ?: 0
+    val maxX = painting.keys.map { it.x }.max() ?: 0
+    val minY = painting.keys.map { it.y }.min() ?: 0
+    val maxY = painting.keys.map { it.y }.max() ?: 0
+    return (minY..maxY)
+        .joinToString(separator = System.lineSeparator()) { y ->
+            (minX..maxX)
+                .map { x -> painting[c(x, y)] ?: BLACK }
+                .joinToString(separator = "") { color ->
+                    if (color == BLACK) " " else "#"
+                }
+        }
+}
+
 fun main() {
     val program = loadProgram("./input/day11.txt")
-    val computer = QueueComputer(program)
-    val robot = Robot(computer)
-    robot.start()
+    val brain = QueueComputer(program)
 
-    println("Part 1: ${robot.painting.size}")
+    val robot1 = Robot(brain = brain, initialColor = BLACK).apply { start() }
+    println("Part 1: ${robot1.painting.size}")
+
+    val robot2 = Robot(brain = brain, initialColor = WHITE).apply { start() }
+    println("Part 2:")
+    println(renderPainting(robot2.painting))
 }
